413acf480f760164c214082d444e8e77caadb3da
kktsch <haldun.koktas@gmail.com>
2021-01-17 21:34:02 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,14 @@
 # load and subset data
 data <- read.delim("C:\\Users\\Haldun\\Desktop\\Exploratory Data Analysis\\Project 1\\household_power_consumption.txt", header=T, sep=";")
 data$Date <- as.Date(data$Date, format= "%m/%d/%Y")
-subsetted_data <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+subsetted_data <- subset(data, Date >= "2006-02-01" & Date <= "2007-02-02")
 clean_data <- subsetted_data[which(subsetted_data$Voltage != "?"),] #missing value removal
 
 # Add weekdays column
+clean_data$Weekdays <- weekdays(clean_data$Date)
 
 #Plotting
-hist(as.integer(as.character(clean_data$Global_active_power)), xlab="Global Active Power (kilowatts)", col="red", main="Global Active Power")
+plot((as.character(clean_data$Global_active_power)), type="l", ylab="Global Active Power (kilowatts)")
 
 #Save
 png(file="C:\\Users\\Haldun\\Desktop\\Exploratory Data Analysis\\Project 1\\plot2.png", width=480, height=480)
